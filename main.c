c96c3d2e026df57d4fa510287ff95cb9bf3a145b
Celso Ernani <celsoernane@gmail.com>
2018-09-04 23:53:37 -0300

    Open file image

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    //parametrizar argv
+    FILE *file = fopen("C:\\Users\\celso\\Desktop\\CELSO\\FACULDADE- IFCE\\s6\\SEMB\\PixelAveraging\\download.jpg", "rb");
+
+   if (!file)
+	{
+		printf("File not found!\n");
+		exit(1);
+	}
+
+	else{
+        printf("Imagem linda com sucesso !");
+ 	}
+
     return 0;
 }
